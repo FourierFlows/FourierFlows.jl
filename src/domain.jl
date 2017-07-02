ba60621f1216c6ff1dba81c87c4da15d6ae271d4
Navid Constantinou <navidcy@gmail.com>
2017-07-01 23:33:51 -0400

    add tests for fft of sin(mx)sin(ny)

--- src/domain.jl
+++ src/domain.jl
@@ -1,3 +1,5 @@
+__precompile__()
+
 module Domain
 
 export Grid
--- tests/basic_tests.jl
+++ tests/basic_tests.jl
@@ -1,23 +1,107 @@
 include("../src/domain.jl")
 
 using Domain
-using PyPlot
+# using PyPlot
 
 
 nx = 8;
-Lx = 2.0*pi;
+Lx = 2.0;
 
 g = Grid(nx, Lx);
+
+################################################################################
+# Test that the X & Y grids are actually created
 
 if maximum(abs.(g.X))<1e-10
   info("X grid is ALL zeros!")
 else
-  println("X grid seems OK.")
+  println("X  grid seems OK.")
 end
-
 
 if maximum(abs.(g.Y))<1e-10
   info("Y grid is ALL zeros!")
 else
-  println("Y grid seems OK.")
+  println("Y  grid seems OK.")
+end
+
+
+################################################################################
+# Test that K, L, Kr, Lr grids are created in the correct order
+
+test = 0;
+for i in g.krange
+  test += abs(maximum(abs.(g.K[i,:]))-minimum(abs.(g.K[i,:])));
+  #the inner abs's are needed because Greg defined K as Complex128
+end
+if test>1e-12
+  info("K grid is not correcty oriented!")
+else
+  println("K  grid seems OK.")
+end
+
+test = 0;
+for i in g.lrange
+  test += abs(maximum(abs.(g.L[:,i]))-minimum(abs.(g.L[:,i])));
+  #the inner abs's are needed because Greg defined L as Complex128
+end
+if test>1e-12
+  info("L grid is not correcty oriented!")
+else
+  println("L  grid seems OK.")
+end
+
+
+test = 0;
+for i in g.krrange
+  test += abs(maximum(abs.(g.Kr[i,:]))-minimum(abs.(g.Kr[i,:])));
+  #the inner abs's are needed because Greg defined Kr as Complex128
+end
+if test>1e-12
+  info("Kr grid is not correcty oriented!")
+else
+  println("Kr grid seems OK.")
+end
+
+################################################################################
+# Test fft's are giving what you expect them to give
+
+if nx/2>=4
+
+  m = 2;
+  n = 1;
+
+  f   = cos.(m*real(g.k[2])*g.X).*cos.(n*real(g.l[2])*g.Y);
+  fh  = fft(f);
+  fhr = rfft(f);
+
+  fh_th  = zeros(size(fh));
+  for i in g.krange
+    for j in g.lrange
+      if ( abs(g.K[i,j])==m*real(g.k[2]) && abs(g.L[i,j])==n*real(g.l[2]) )
+        fh_th[i,j] = - nx^2/4;
+      end
+    end
+  end
+
+  fhr_th  = zeros(size(fhr));
+  for i in g.krrange
+    for j in g.lrange
+      if ( abs(g.Kr[i,j])==m*real(g.k[2]) && abs(g.L[i,j])==n*real(g.l[2]) )
+        fhr_th[i,j] = - nx^2/4;
+      end
+    end
+  end
+
+  if norm(fh-fh_th)>1e-14
+    info("fft  for sin(mx)sin(ny) is not correcty calculated!")
+  else
+    println("fft  for sin(mx)sin(ny) seems OK.")
+  end
+
+  if norm(fhr-fhr_th)>1e-14
+    info("rfft for sin(mx)sin(ny) is not correcty calculated!")
+  else
+    println("rfft for sin(mx)sin(ny) seems OK.")
+  end
+
 end
