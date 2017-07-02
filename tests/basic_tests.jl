6708c3d64b931c779d3be6b3c9daa2be0f0b720b
Navid Constantinou <navidcy@gmail.com>
2017-07-01 22:26:56 -0400

    add Y grid basic test

--- tests/basic_tests.jl
+++ tests/basic_tests.jl
@@ -4,7 +4,7 @@ using Domain
 using PyPlot
 
 
-nx = 128;
+nx = 8;
 Lx = 2.0*pi;
 
 g = Grid(nx, Lx);
@@ -13,4 +13,11 @@ if maximum(abs.(g.X))<1e-10
   info("X grid is ALL zeros!")
 else
   println("X grid seems OK.")
+end
+
+
+if maximum(abs.(g.Y))<1e-10
+  info("Y grid is ALL zeros!")
+else
+  println("Y grid seems OK.")
 end
