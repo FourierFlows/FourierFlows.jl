33ad4993fe636065a61494070ce915789eac1a45
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2017-07-11 20:37:00 -0400

    Adds plotting

--- tests/barotropicqg/gaussianmountain.jl
+++ tests/barotropicqg/gaussianmountain.jl
@@ -1,6 +1,6 @@
 include("../../src/physics/barotropicqg.jl")
 
-using BarotropicQG
+using BarotropicQG, PyPlot
 
 # Problem parameters
 nx       = 128
@@ -11,4 +11,10 @@ g, p, v, eq = southern_gaussian_mountain(nx, betastar)
 ts = ETDRK4TimeStepper(fdt*2.0*pi/p.f0, eq.LC)
 
 stepforward!(v, 10, ts, eq, p, g)
+
+fig = figure()
+imshow(v.zet)
+#pcolormesh(g.X, g.Y, v.zet)
+
+
 
