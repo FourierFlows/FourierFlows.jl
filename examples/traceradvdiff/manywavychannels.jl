c0e5a6bdf96a7f3168f2576b0b9727ba141b2d5f
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2017-11-01 16:01:20 -0400

    Changes saving and plotting names

--- examples/traceradvdiff/manywavychannels.jl
+++ examples/traceradvdiff/manywavychannels.jl
@@ -2,39 +2,37 @@ include("./wavychannel.jl")
 
 using JLD2
 
-filename = "manyeps.jld2"
-
-nx  = 1024
+nx  = 512 
 kap = 1e-4
+eta = 1e-6
 dcx = 0.01
 dcy = 0.02
 
+
 epz = 0.0:0.1:0.9
 nepz = length(epz)
-enhancement = zeros(nepz)
+
+diffratio = zeros(nepz)
 
 for i = 1:nepz
 
   ep = epz[i]
 
-  prob, yvar = wavy_barotropic_tracer_run(ep, kap; nx=nx, dcx=dcx, dcy=dcy)
+  prob, yvar = wavy_barotropic_tracer_run(ep, kap; nx=nx, dcx=dcx, dcy=dcy, 
+    CFL=0.1, eta=eta)
 
   yvar_theory = 2*kap*yvar.time[end] + dcy^2.0
 
-  groupname = @sprintf("ep%02d", Int(ep*10))
-  jldopen(filename, "a+") do file
-    file["$groupname/yvar"] = yvar
-    file["$groupname/c"] = prob.vars.c
-    file["$groupname/x"] = prob.grid.X
-    file["$groupname/y"] = prob.grid.Y
-  end
+  diffratio[i] = yvar.value / yvar_theory
+  println("diffusivity ratio: ", diffratio[i], "\n\n")
 
-  enhancement[i] = yvar.value / yvar_theory
-  println("enhancement: ", enhancement[i], "\n\n")
+  time, variance = yvar.time, yvar.data
+  c, X, Y = prob.vars.c, prob.grid.X, prob.grid.Y
+
+  filename = @sprintf("wavychannel_nx%04d_ep%02d.jld2", Int(ep*10))
+  @save filename time variance c X Y
 
 end
 
-
-jldopen(filename, "a+") do file
-  file["enhnacement"] = enhancement
-end
+finalfilename = @sprintf("manyeps_nx%04d.jld2", nx)
+@save finalfilename epz diffratio
