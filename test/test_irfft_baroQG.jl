10ff8b74e6b7599cd2aa952e51e3dcc37c23b575
Navid Constantinou <navidcy@gmail.com>
2017-11-02 21:37:33 -0700

    renames test filenames

--- test/test_irfft_baroQG.jl
+++ test/test_irfft_baroQG.jl
@@ -1,9 +1,42 @@
 include("../src/fourierflows.jl")
 
 using FourierFlows,
-    #   PyPlot,
       FourierFlows.BarotropicQG
 
+
+module IRFFTbaroQGtests
+
+""" Test whether the irfft's are implemented correctly by evolving a random
+initial condition for dt=1e-15 looking at relative error of the norm. """
+function test_baroQG_irfft(grid, params, vars, eq)
+
+    # a random initial condition
+    zeta0 = randn(grid.nx, grid.nx)
+    zeta0 = zeta0 - mean(zeta0)
+
+    set_zeta!(vars, params, grid, zeta0)
+
+    ts = ETDRK4TimeStepper(dt, eq.LC)
+    # ts = ForwardEulerTimeStepper(dt, eq.LC)
+
+    nloops = 1
+    nsteps = 1
+
+    @time stepforward!(vars, ts, eq, params, grid, nsteps=1)
+    BarotropicQG.updatevars!(vars, params, grid)
+    println(norm(zeta0-v.zeta)/norm(zeta0))
+
+    norm(zeta0-v.zeta)/norm(zeta0) < 1.0e-14
+end
+
+
+end # end IRFFTbaroQGtests
+
+
+import IRFFTbaroQGtests: test_baroQG_irfft
+
+
+# Run tests
 nx  = 128
 dt  = 1e-17
 nu  = 1e-6
@@ -18,38 +51,4 @@ p  = FreeDecayParams(g, beta, 0.0, mu, nu, nun)
 v  = FreeDecayVars(g)
 eq = Equation(p, g)
 
-# a random initial condition
-zeta0 = randn(nx, nx)
-zeta0 = zeta0 - mean(zeta0)
-
-set_zeta!(v, p, g, zeta0)
-
-ts = ETDRK4TimeStepper(dt, eq.LC)
-# ts = ForwardEulerTimeStepper(dt, eq.LC)
-
-# fig, axs = subplots(nrows=1, ncols=2, sharey=true, sharex=true)
-#
-# function test_plot(g, v, zeta0, fig, axs; iter=1)
-#   # Make a plot that compared two-dimensional turbulence solved by
-#   # the TwoDTurb and BarotropicQG modules.
-#
-#   im1 = axs[1][:pcolormesh](g.X, g.Y, v.zeta)
-#   im2 = axs[2][:pcolormesh](g.X, g.Y, abs.(v.zeta-zeta0))
-#   # axis("square")
-#   if iter == 1
-#     colorbar(im1, ax=axs[1])
-#     colorbar(im2, ax=axs[2])
-#   end
-#   pause(0.01)
-# end
-
-
-nloops = 1
-nsteps = 1
-
-@time stepforward!(v, ts, eq, p, g, nsteps=1)
-BarotropicQG.updatevars!(v, p, g)
-# test_plot(g, v, zeta0, fig, axs; iter=1)
-
-test = norm(zeta0-v.zeta)/norm(zeta0) < 1.0e-14
-println("test_baroQG_irfft:    ", test)
+println("test_baroQG_irfft:    ", test_baroQG_irfft(g, p, v, eq))
