506a0201273f2cc30af1389bd2e6b82097ca18b6
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2017-11-08 14:06:58 -0500

    Updates

--- examples/twomodeboussinesq/eddywave/movies/two_component_movie.jl
+++ examples/twomodeboussinesq/eddywave/movies/two_component_movie.jl
@@ -3,20 +3,17 @@ include("../../../../src/fourierflows.jl")
 using FourierFlows, PyPlot, PyCall, JLD2
 
 import FourierFlows.TwoModeBoussinesq
-
-import FourierFlows.TwoModeBoussinesq: mode0apv, mode1apv, mode1speed, mode1w, 
-  wave_induced_speed, wave_induced_psi, wave_induced_uv, lagrangian_mean_uv,
-  calc_chi, calc_chi_uv, totalenergy, mode0energy, mode1energy, CFL
+import FourierFlows.TwoModeBoussinesq: mode1u, mode1w 
 
 @pyimport mpl_toolkits.axes_grid1 as pltgrid
 
-plotname = "./plots/Z-w"
-filename = "../data/nk32_nu1e+32_122f_n512_ep20_Ro05_nkw32.jld2"
+plotname = "./plots/introduction"
+filename = "../data/example_nk64_nu2e+32_122f_n1024_ep20_Ro05_nkw64.jld2"
 
 # Plot parameters
 eddylim = 10
 Ro0 = 0.04
-w0  = 0.08
+u0  = 4.00
 
 # Recreate problem
 jldopen(filename, "r") do file
@@ -37,12 +34,12 @@ jldopen(filename, "r") do file
     nx=n, Lx=L, ν0=ν0, nν0=nν0, ν1=ν1, nν1=nν1, f=f, N=N, m=m, dt=dt)
 
   # Non-retrievable parameters
-  nkw = 32
+  nkw = 64
    kw = 2π*nkw/L
     α = (N*kw)^2/(f*m)^2
     σ = f*sqrt(1+α)
    tσ = 2π/σ
-   Re = L/40
+   Re = L/80
 
   x, y = prob.grid.X/Re, prob.grid.Y/Re
 
@@ -60,25 +57,23 @@ jldopen(filename, "r") do file
     TwoModeBoussinesq.set_Z!(prob, Z)
     TwoModeBoussinesq.set_uvp!(prob, u, v, p)
     
-    w = mode1w(prob)
-    Q = mode0apv(prob)
+    Umax = maximum(sqrt.(prob.vars.U.^2.0+prob.vars.V.^2.0))
 
-    println(step)
+    u = mode1u(prob)/Umax
+
 
     close("all")
     fig, axs = subplots(ncols=2, nrows=1, sharex=true, sharey=true, 
       figsize=(8, 5))
 
     axes(axs[1]); axis("equal")
-    Qplot = pcolormesh(x, y, Z/f, 
-      cmap="RdBu_r", vmin=-Ro0, vmax=Ro0)
+    Zplot = pcolormesh(x, y, Z/f, cmap="RdBu_r", vmin=-Ro0, vmax=Ro0)
 
     axes(axs[2]); axis("equal")
-    wplot = pcolormesh(x, y, w, 
-      cmap="RdBu_r", vmin=-w0, vmax=w0)
+    uplot = pcolormesh(x, y, u, cmap="RdBu_r", vmin=-u0, vmax=u0)
 
 
-    plots = [Qplot, wplot]
+    plots = [Zplot, uplot]
     cbs = []
     for (i, ax) in enumerate(axs)
       ax[:set_adjustable]("box-forced")
@@ -87,7 +82,7 @@ jldopen(filename, "r") do file
       ax[:tick_params](axis="both", which="both", length=0)
 
       divider = pltgrid.make_axes_locatable(ax)
-      cax = divider[:append_axes]("top", size="5%", pad="5%")
+      cax = divider[:append_axes]("top", size="5%", pad="8%")
       cb = colorbar(plots[i], cax=cax, orientation="horizontal")
 
       cb[:ax][:xaxis][:set_ticks_position]("top")
@@ -102,23 +97,29 @@ jldopen(filename, "r") do file
     axs[2][:set_xlabel](L"x/R")
     axs[1][:set_ylabel](L"y/R")
 
+    ticks = [-8, -4, 0, 4, 8]
+    axs[1][:xaxis][:set_ticks](ticks)
+    axs[1][:yaxis][:set_ticks](ticks)
+    axs[2][:xaxis][:set_ticks](ticks)
+
     cbs[1][:ax][:xaxis][:set_ticks_position]("top")
     cbs[1][:ax][:xaxis][:set_label_position]("top")
 
     cbs[1][:set_ticks]([-Ro0, 0.0, Ro0])
-    cbs[2][:set_ticks]([-w0, 0.0, w0])
+    cbs[2][:set_ticks]([-u0, 0.0, u0])
 
-    cbs[1][:set_label](L"Ro = Z/f", labelpad=12.0)
-    cbs[2][:set_label](L"\hat w(z=0) = w + w^*", labelpad=12.0)
+    labelpad = 16.0
+    cbs[1][:set_label]("Barotropic vorticity \$= Z/f\$", labelpad=labelpad)
+    cbs[2][:set_label]("Normalized \$\\hat u(z=0) = (u + u^*)/\\max(U)\$", 
+      labelpad=labelpad)
 
     msg = @sprintf("\$t = %02d\$ wave periods", t/tσ)
-    figtext(0.51, 0.9, msg, horizontalalignment="center", fontsize=14)
+    figtext(0.51, 0.95, msg, horizontalalignment="center", fontsize=14)
 
     tight_layout(rect=(0.00, 0.00, 0.95, 0.90), h_pad=0.05)
 
-    #pause(0.1)
-    
     savename = @sprintf("%s_%06d.png", plotname, istep)
+    println(savename)
     savefig(savename, dpi=240)
   end
 
--- examples/twomodeboussinesq/eddywave/movies/wave_induced_flow_movie.jl
+++ examples/twomodeboussinesq/eddywave/movies/wave_induced_flow_movie.jl
@@ -4,21 +4,19 @@ using FourierFlows, PyPlot, PyCall, JLD2
 
 import FourierFlows.TwoModeBoussinesq
 
-import FourierFlows.TwoModeBoussinesq: mode0apv, mode1apv, mode1speed, mode1w, 
-  wave_induced_speed, wave_induced_psi, wave_induced_uv, lagrangian_mean_uv,
-  calc_chi, calc_chi_uv, totalenergy, mode0energy, mode1energy, CFL,
-  lagrangian_mean_psi, apv_induced_psi
+import FourierFlows.TwoModeBoussinesq: mode0apv, mode1apv, mode1u, mode1w, 
+  calc_chi, calc_chi_uv, apv_induced_psi
+  
 
 @pyimport mpl_toolkits.axes_grid1 as pltgrid
 
-simname = "nk32_nu1e+32_122f_n512_ep20_Ro05_nkw32"
 plotname = "./plots/wif"
-filename = "../data/$simname.jld2"
+filename = "../data/example_nk64_nu2e+32_122f_n1024_ep20_Ro05_nkw64.jld2"
 
 # -- Plot parameters --
   eddylim = 10
       Ro0 = 0.04
-       w0 = 0.08
+       u0 = 4.00
       sp0 = 1.0
 ncontours = 10
 
@@ -42,12 +40,12 @@ jldopen(filename, "r") do file
     nx=n, Lx=L, ν0=ν0, nν0=nν0, ν1=ν1, nν1=nν1, f=f, N=N, m=m, dt=dt)
 
   # Non-retrievable parameters
-  nkw = 32
+  nkw = 64
    kw = 2π*nkw/L
     α = (N*kw)^2/(f*m)^2
     σ = f*sqrt(1+α)
    tσ = 2π/σ
-   Re = L/40
+   Re = L/80
 
   x, y = prob.grid.X/Re, prob.grid.Y/Re
 
@@ -67,7 +65,7 @@ jldopen(filename, "r") do file
 
     Umax = maximum(sqrt.(prob.vars.U.^2+prob.vars.V.^2))
     
-    w = mode1w(prob)
+    u = mode1u(prob)/Umax
     Q = mode0apv(prob)
     PsiQ = apv_induced_psi(prob)
 
@@ -75,7 +73,6 @@ jldopen(filename, "r") do file
     uc, vc = calc_chi_uv(prob)
     spc = sqrt.(uc.^2.0+vc.^2.0)/Umax
 
-    println(step)
 
 
     close("all")
@@ -84,23 +81,17 @@ jldopen(filename, "r") do file
 
 
     axes(axs[1]); axis("equal")
-    plot1 = pcolormesh(x, y, Q/f, 
-      cmap="RdBu_r", vmin=-Ro0, vmax=Ro0)
+    plot1 = pcolormesh(x, y, Q/f, cmap="RdBu_r", vmin=-Ro0, vmax=Ro0)
     contour(x, y, PsiQ, ncontours, colors="k", linewidths=0.5, alpha=0.3)
 
 
-
     axes(axs[2]); axis("equal")
-    plot2 = pcolormesh(x, y, spc, 
-      cmap="YlGnBu_r", vmin=0.0, vmax=sp0)
+    plot2 = pcolormesh(x, y, spc, cmap="YlGnBu_r", vmin=0.0, vmax=sp0)
     contour(x, y, Chi, ncontours, colors="w", linewidths=0.5, alpha=0.3)
 
 
-
     axes(axs[3]); axis("equal")
-    plot3 = pcolormesh(x, y, w, 
-      cmap="RdBu_r", vmin=-w0, vmax=w0)
-
+    plot3 = pcolormesh(x, y, u, cmap="RdBu_r", vmin=-u0, vmax=u0)
 
 
     plots = [plot1, plot2, plot3]
@@ -112,7 +103,7 @@ jldopen(filename, "r") do file
       ax[:tick_params](axis="both", which="both", length=0)
 
       divider = pltgrid.make_axes_locatable(ax)
-      cax = divider[:append_axes]("top", size="5%", pad="5%")
+      cax = divider[:append_axes]("top", size="5%", pad="8%")
       cb = colorbar(plots[i], cax=cax, orientation="horizontal")
 
       cb[:ax][:xaxis][:set_ticks_position]("top")
@@ -128,22 +119,31 @@ jldopen(filename, "r") do file
     axs[3][:set_xlabel](L"x/R")
     axs[1][:set_ylabel](L"y/R")
 
+    ticks = [-8, -4, 0, 4, 8]
+    axs[1][:xaxis][:set_ticks](ticks)
+    axs[1][:yaxis][:set_ticks](ticks)
+    axs[2][:xaxis][:set_ticks](ticks)
+    axs[3][:xaxis][:set_ticks](ticks)
+
     cbs[1][:set_ticks]([-Ro0, 0.0, Ro0])
     cbs[2][:set_ticks]([0.0, sp0])
-    cbs[3][:set_ticks]([-w0, 0.0, w0])
+    cbs[3][:set_ticks]([-u0, 0.0, u0])
 
-    cbs[1][:set_label]("\$Q/f\$ and \$\\Phi\$", labelpad=12.0)
-    cbs[2][:set_label]("\$| \\nabla \\chi |\$ and \$\\chi\$", labelpad=12.0)
-    cbs[3][:set_label](L"\hat w(z=0) = w + w^*", labelpad=12.0)
+    labelpad = 16.0
+    cbs[1][:set_label]("\$Q/f\$ and \$\\Phi\$", labelpad=labelpad)
+    cbs[2][:set_label]("\$| \\nabla \\chi |\$ and \$\\chi\$", 
+      labelpad=labelpad)
+    cbs[3][:set_label](L"(u + u^*)/\max(U)", labelpad=labelpad)
 
     msg = @sprintf("\$t = %02d\$ wave periods", t/tσ)
     figtext(0.51, 0.95, msg, horizontalalignment="center", fontsize=14)
 
-    tight_layout(rect=(0.00, 0.00, 0.95, 0.90), h_pad=0.05)
+    tight_layout(rect=(0.10, 0.05, 0.90, 0.85), h_pad=0.05)
 
     #pause(0.1)
     
     savename = @sprintf("%s_%06d.png", plotname, istep)
+    println(savename)
     savefig(savename, dpi=240)
   end
 
