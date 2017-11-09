b33aa89bd7ea8ecc231d62d5aab6eecf29a3ab0e
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2017-11-08 20:47:59 -0500

    Adds a new vars and calcNL to integrate barotropic APV instead of barotropic vorticity

--- src/physics/twomodeboussinesq.jl
+++ src/physics/twomodeboussinesq.jl
@@ -51,6 +51,43 @@ end
 
 
 
+"""
+Returns a TwoModeBoussinesq problem in which the APV is used
+instead of vorticity as the barotropic prognostic variable.
+"""
+function PrognosticAPVInitialValueProblem(;
+  nx   = 128, 
+  Lx   = 2pi, 
+  ny   = nothing,
+  Ly   = nothing,
+  ν0   = nothing, 
+  nν0  = 2, 
+  ν1   = nothing,
+  nν1  = 2, 
+  f    = 1.0,
+  N    = 10.0,
+  m    = 40.0,
+  Us   = 0.0,
+  Vs   = 0.0,
+  dt   = 0.01
+  )
+
+  if Ly == nothing; Ly = Lx; end
+  if ny == nothing; ny = nx; end
+  if ν0 == nothing; ν0 = 1e-1/(dt*(0.65*pi*nx/Lx)^nν0); end
+  if ν1 == nothing; ν1 = 1e-1/(dt*(0.65*pi*nx/Lx)^nν1); end
+
+  g  = TwoDGrid(nx, Lx)
+  pr = TwoModeBoussinesq.PrognosticAPVParams(ν0, nν0, ν1, nν1, f, N, m)
+  vs = TwoModeBoussinesq.PrognosticAPVVars(g)
+  eq = TwoModeBoussinesq.Equation(pr, g)
+  ts = ETDRK4TimeStepper(dt, eq.LCc, eq.LCr)
+
+  FourierFlows.Problem(g, vs, pr, eq, ts)
+end
+
+
+
 
 function PassiveAPVInitialValueProblem(;
   nx  = 128, 
@@ -109,6 +146,29 @@ end
 
 
 
+"""
+Defines a type for problems in which the baroclinic APV is zero, allowing
+for the barotropic dynamics to be solved by evolving a barotropic APV 
+equation.
+"""
+type PrognosticAPVParams <: TwoModeParams
+  ν0::Float64                 # Mode-0 viscosity
+  nν0::Int                    # Mode-0 hyperviscous order
+  ν1::Float64                 # Mode-1 viscosity
+  nν1::Int                    # Mode-1 hyperviscous order
+  f::Float64                  # Planetary vorticity
+  N::Float64                  # Buoyancy frequency
+  m::Float64                  # Mode-one wavenumber
+  Us::Float64                 # Steady mode-0 mean x-velocity
+  Vs::Float64                 # Steady mode-0 mean y-velocity
+end
+
+function PrognosticAPVParams(ν0, nν0::Int, ν1, nν1::Int, f, N, m)
+  PrognosticAPVParams(ν0, nν0, ν1, nν1, f, N, m, 0.0, 0.0)
+end
+
+
+
 
 type PassiveAPVParams <: TwoModeParams
   _params::Params
@@ -146,7 +206,6 @@ function Equation(p::TwoModeParams, g::TwoDGrid)
   LCc = zeros(g.nk, g.nl, 3)
   LCc[:, :, 1] = -p.ν1 * g.KKsq.^(0.5*p.nν1)
   LCc[:, :, 2] = -p.ν1 * g.KKsq.^(0.5*p.nν1)
-  #LCc[:, :, 3] = -p.ν1 * g.KKsq.^(0.5*p.nν1)
 
   Equation(LCc, LCr, calcNL!)
 end
@@ -313,6 +372,149 @@ function Vars(g::TwoDGrid)
     uh, vh, wh, ph, zetah, Uuh, Uvh, Uph, Vuh, Vvh, Vph, uUxvUyh, uVxvVyh,
     )
 end
+
+
+
+
+"""
+Defines a type for problems in which the barotropic APV is used as the 
+prognostic variable and the baroclinic APV is assumed equal to zero.
+"""
+type PrognosticAPVVars <: TwoModeVars
+  t::Float64
+  solr::Array{Complex128, 2}
+  solc::Array{Complex128, 3}
+
+  # Auxiliary zeroth-mode vars
+  Q::Array{Float64, 2}
+  U::Array{Float64, 2}
+  V::Array{Float64, 2}
+  Ux::Array{Float64, 2}
+  Uy::Array{Float64, 2}
+  Vx::Array{Float64, 2}
+  Vy::Array{Float64, 2}
+  UQ::Array{Float64, 2}
+  VQ::Array{Float64, 2}
+  up::Array{Float64, 2}
+  vp::Array{Float64, 2}
+  Psi::Array{Float64, 2}
+
+  # Auxiliary first-mode vars
+  u::Array{Complex{Float64}, 2}
+  v::Array{Complex{Float64}, 2}
+  p::Array{Complex{Float64}, 2}
+
+  # Multiplies
+  Uu::Array{Complex{Float64}, 2}
+  Uv::Array{Complex{Float64}, 2}
+  Up::Array{Complex{Float64}, 2}
+  Vu::Array{Complex{Float64}, 2}
+  Vv::Array{Complex{Float64}, 2}
+  Vp::Array{Complex{Float64}, 2}
+  uUxvUy::Array{Complex{Float64}, 2}
+  uVxvVy::Array{Complex{Float64}, 2}
+
+  # Zeroth-mode transforms
+  Qh::Array{Complex{Float64}, 2}
+  Uh::Array{Complex{Float64}, 2}
+  Vh::Array{Complex{Float64}, 2}
+  Uxh::Array{Complex{Float64}, 2}
+  Uyh::Array{Complex{Float64}, 2}
+  Vxh::Array{Complex{Float64}, 2}
+  Vyh::Array{Complex{Float64}, 2}
+  UQh::Array{Complex{Float64}, 2}
+  VQh::Array{Complex{Float64}, 2}
+  uph::Array{Complex{Float64}, 2}
+  vph::Array{Complex{Float64}, 2}
+  Psih::Array{Complex{Float64}, 2}
+
+  # First-mode transforms
+  uh::Array{Complex{Float64}, 2}
+  vh::Array{Complex{Float64}, 2}
+  ph::Array{Complex{Float64}, 2}
+
+  # Multiply transforms
+  Uuh::Array{Complex{Float64}, 2}
+  Uvh::Array{Complex{Float64}, 2}
+  Uph::Array{Complex{Float64}, 2}
+  Vuh::Array{Complex{Float64}, 2}
+  Vvh::Array{Complex{Float64}, 2}
+  Vph::Array{Complex{Float64}, 2}
+  uUxvUyh::Array{Complex{Float64}, 2}
+  uVxvVyh::Array{Complex{Float64}, 2}
+end
+
+function PrognosticAPVVars(g::TwoDGrid)
+  # Initialize with t=0
+  t = 0.0
+  solc = zeros(Complex{Float64}, g.nk, g.nl, 3)
+  solr = zeros(Complex{Float64}, g.nkr, g.nl)
+
+  # Auxiliary zeroth-mode vars
+  Q      = zeros(Float64, g.nx, g.ny)
+  U      = zeros(Float64, g.nx, g.ny)
+  V      = zeros(Float64, g.nx, g.ny)
+  Ux     = zeros(Float64, g.nx, g.ny)
+  Uy     = zeros(Float64, g.nx, g.ny)
+  Vx     = zeros(Float64, g.nx, g.ny)
+  Vy     = zeros(Float64, g.nx, g.ny)
+  UQ     = zeros(Float64, g.nx, g.ny)
+  VQ     = zeros(Float64, g.nx, g.ny)
+  up     = zeros(Float64, g.nx, g.ny)
+  vp     = zeros(Float64, g.nx, g.ny)
+  Psi    = zeros(Float64, g.nx, g.ny)
+  
+  # Auxiliary first-mode vars
+  u      = zeros(Complex{Float64}, g.nx, g.ny)
+  v      = zeros(Complex{Float64}, g.nx, g.ny)
+  p      = zeros(Complex{Float64}, g.nx, g.ny)
+
+  Uu     = zeros(Complex{Float64}, g.nx, g.ny)
+  Uv     = zeros(Complex{Float64}, g.nx, g.ny)
+  Up     = zeros(Complex{Float64}, g.nx, g.ny)
+  Vu     = zeros(Complex{Float64}, g.nx, g.ny)
+  Vv     = zeros(Complex{Float64}, g.nx, g.ny)
+  Vp     = zeros(Complex{Float64}, g.nx, g.ny)
+
+  uUxvUy = zeros(Complex{Float64}, g.nx, g.ny)
+  uVxvVy = zeros(Complex{Float64}, g.nx, g.ny)
+
+  # Transforms
+  Qh     = zeros(Complex{Float64}, g.nkr, g.nl)
+  Uh     = zeros(Complex{Float64}, g.nkr, g.nl)
+  Vh     = zeros(Complex{Float64}, g.nkr, g.nl)
+  Uxh    = zeros(Complex{Float64}, g.nkr, g.nl)
+  Uyh    = zeros(Complex{Float64}, g.nkr, g.nl)
+  Vxh    = zeros(Complex{Float64}, g.nkr, g.nl)
+  Vyh    = zeros(Complex{Float64}, g.nkr, g.nl)
+  UQh    = zeros(Complex{Float64}, g.nkr, g.nl)
+  VQh    = zeros(Complex{Float64}, g.nkr, g.nl)
+  uph    = zeros(Complex{Float64}, g.nkr, g.nl)
+  vph    = zeros(Complex{Float64}, g.nkr, g.nl)
+  Psih   = zeros(Complex{Float64}, g.nkr, g.nl)
+
+  uh     = zeros(Complex{Float64}, g.nk, g.nl)
+  vh     = zeros(Complex{Float64}, g.nk, g.nl)
+  ph     = zeros(Complex{Float64}, g.nk, g.nl)
+  
+  Uuh    = zeros(Complex{Float64}, g.nk, g.nl)
+  Uvh    = zeros(Complex{Float64}, g.nk, g.nl)
+  Uph    = zeros(Complex{Float64}, g.nk, g.nl)
+  Vuh    = zeros(Complex{Float64}, g.nk, g.nl)
+  Vvh    = zeros(Complex{Float64}, g.nk, g.nl)
+  Vph    = zeros(Complex{Float64}, g.nk, g.nl)
+
+  uUxvUyh= zeros(Complex{Float64}, g.nk, g.nl)
+  uVxvVyh= zeros(Complex{Float64}, g.nk, g.nl)
+
+  return PrognosticAPVVars(t, solr, solc, 
+    Q, U, V, Ux, Uy, Vx, Vy, UQ, VQ, up, vp, Psi, 
+    u, v, p, Uu, Uv, Up, Vu, Vv, Vp, uUxvUy, uVxvVy,
+    Qh, Uh, Vh, Uxh, Uyh, Vxh, Vyh, UQh, VQh, uph, vph, Psih, 
+    uh, vh, ph, Uuh, Uvh, Uph, Vuh, Vvh, Vph, uUxvUyh, uVxvVyh,
+    )
+end
+
 
 
 
@@ -538,7 +740,6 @@ function calcNL!(
   v.Vh[1, 1] += p.Vs*g.nx*g.ny
 
   @views @. v.zetah = im*g.K*solc[:, :, 2] - im*g.L*solc[:, :, 1]
-
  
   # Inverse transforms
   A_mul_B!(v.Z, g.irfftplan, v.Zh)
@@ -620,6 +821,122 @@ function calcNL!(
 end
 
 
+
+
+""" 
+Calculate the streamfunction of the barotropic flow.
+"""
+function calcPsih!(
+  v::PrognosticAPVVars, p::PrognosticAPVParams, g::TwoDGrid,
+  solc::Array{Complex{Float64}, 3}, solr::Array{Complex{Float64}, 2})
+  
+  @views A_mul_B!(v.u, g.ifftplan, solc[:, :, 1])
+  @views A_mul_B!(v.v, g.ifftplan, solc[:, :, 2])
+  @views A_mul_B!(v.p, g.ifftplan, solc[:, :, 3])
+
+  @. v.up = real(v.u*conj(v.p) + conj(v.u)*v.p)
+  @. v.vp = real(v.v*conj(v.p) + conj(v.v)*v.p)
+
+  A_mul_B!(v.uph, g.rfftplan, v.up)
+  A_mul_B!(v.vph, g.rfftplan, v.vp)
+
+  @. v.Psih = -g.invKKrsq*(solr 
+    + im*p.m^2.0/p.N^2.0*(g.Kr*v.vph - g.Lr*v.uph))
+
+  nothing
+end
+
+
+
+
+"""
+Calculate the nonlinear term for a problem where the prognostic barotropic
+quantity is available potential vorticity under the assumption that the
+baroclinic available potential vorticity is zero.
+"""
+function calcNL!(
+  NLc::Array{Complex{Float64}, 3},  NLr::Array{Complex{Float64}, 2}, 
+  solc::Array{Complex{Float64}, 3}, solr::Array{Complex{Float64}, 2}, 
+  t::Float64, v::PrognosticAPVVars, p::PrognosticAPVParams, g::TwoDGrid)
+
+  calcPsih!(v, p, g, solc, solr)
+
+  @. v.Uh = -im*g.Lr*v.Psih
+  @. v.Vh =  im*g.Kr*v.Psih
+
+  @. v.Uxh = im*g.Kr*v.Uh
+  @. v.Vxh = im*g.Kr*v.Vh
+
+  @. v.Uyh = im*g.Lr*v.Uh
+  @. v.Vyh = im*g.Lr*v.Vh
+
+  v.Uh[1, 1] += p.Us*g.nx*g.ny
+  v.Vh[1, 1] += p.Vs*g.nx*g.ny
+
+  # Inverse transforms
+  v.Qh .= solr
+  A_mul_B!(v.Q, g.irfftplan, v.Qh)
+  A_mul_B!(v.U, g.irfftplan, v.Uh)
+  A_mul_B!(v.V, g.irfftplan, v.Vh)
+
+  A_mul_B!(v.Ux, g.irfftplan, v.Uxh)
+  A_mul_B!(v.Uy, g.irfftplan, v.Uyh)
+  A_mul_B!(v.Vx, g.irfftplan, v.Vxh)
+  A_mul_B!(v.Vy, g.irfftplan, v.Vyh)
+
+  @. v.UQ = v.U * v.Q
+  @. v.VQ = v.V * v.Q
+  @. v.Uu = v.U * v.u
+  @. v.Vu = v.V * v.u
+  @. v.Uv = v.U * v.v
+  @. v.Vv = v.V * v.v
+  @. v.Up = v.U * v.p
+  @. v.Vp = v.V * v.p
+
+  @. v.uUxvUy = v.u*v.Ux + v.v*v.Uy
+  @. v.uVxvVy = v.u*v.Vx + v.v*v.Vy
+
+  # Forward transforms
+  A_mul_B!(v.UQh, g.rfftplan, v.UQ)
+  A_mul_B!(v.VQh, g.rfftplan, v.VQ)
+
+  A_mul_B!(v.Uuh, g.fftplan, v.Uu)
+  A_mul_B!(v.Uvh, g.fftplan, v.Uv)
+  A_mul_B!(v.Vuh, g.fftplan, v.Vu)
+  A_mul_B!(v.Vvh, g.fftplan, v.Vv)
+  A_mul_B!(v.Uph, g.fftplan, v.Up)
+  A_mul_B!(v.Vph, g.fftplan, v.Vp)
+
+  A_mul_B!(v.uUxvUyh, g.fftplan, v.uUxvUy)
+  A_mul_B!(v.uVxvVyh, g.fftplan, v.uVxvVy)
+
+
+  # ---------------------------------------------------------------------------   
+  # Zeroth-mode nonlinear term
+  @. NLr = - im*g.Kr*v.UQh - im*g.Lr*v.VQh
+
+  # First-mode nonlinear terms:
+  # u
+  @views @. NLc[:, :, 1] = ( p.f*solc[:, :, 2] 
+    - im*(g.K*(solc[:, :, 3] + v.Uuh) + g.L*v.Vuh) - v.uUxvUyh
+  )
+
+  # v
+  @views @. NLc[:, :, 2] = ( -p.f*solc[:, :, 1] 
+      - im*(g.L*(solc[:, :, 3] + v.Vvh) + g.K*v.Uvh) - v.uVxvVyh
+  )
+
+  # p
+  @views @. NLc[:, :, 3] = -im*( 
+     g.K*(p.N^2.0/p.m^2.0*solc[:, :, 1] + v.Uph)
+   + g.L*(p.N^2.0/p.m^2.0*solc[:, :, 2] + v.Vph)
+  )
+
+  dealias!(NLr, g)
+  dealias!(NLc, g)
+
+  nothing
+end
 
 
 """ Calculate the nonlinear right side for the two-mode Boussinesq equations
@@ -758,15 +1075,29 @@ function updatevars!(v::PassiveAPVVars, p::PassiveAPVParams, g::TwoDGrid)
   nothing
 end
 
+function updatevars!(v::PrognosticAPVVars, p::TwoModeParams, g::TwoDGrid) 
+
+  calcPsih!(v, p, g, v.solc, v.solr)
+  @. v.Uh = -im*g.Lr*v.Psih
+  @. v.Vh =  im*g.Kr*v.Psih
+ 
+  # Update vars destroys Psih, Uh, Vh, Qh
+  A_mul_B!(v.Psi, g.irfftplan, v.Psih)
+  A_mul_B!(v.U, g.irfftplan, v.Uh)
+  A_mul_B!(v.V, g.irfftplan, v.Vh)
+  A_mul_B!(v.Q, g.irfftplan, v.Qh)
+
+  nothing
+end
+
 function updatevars!(prob::AbstractProblem)
   updatevars!(prob.vars, prob.params, prob.grid)
 end
 
 
 
-
 """
-Set zeroth mode vorticity, zeta, and update vars. 
+Set zeroth mode vorticity and update vars. 
 """
 function set_Z!(v::Vars, p::TwoModeParams, g::TwoDGrid, Z)
   A_mul_B!(v.solr, g.rfftplan, Z)
@@ -795,10 +1126,28 @@ end
 
 
 
+"""
+Set the zeroth mode available potential vorticity.
+"""
+function set_Q!(v::PrognosticAPVVars, p::TwoModeParams, g::TwoDGrid, Q)
+  A_mul_B!(v.solr, g.rfftplan, Q)
+  updatevars!(v, p, g)
+  nothing
+end
+
+function set_Q!(prob::AbstractProblem, Q)
+  set_Q!(prob.vars, prob.params, prob.grid, Q)
+end
+
+
+
+
+
+
 """ 
 Set first mode u, v, and p and update vars.
 """
-function set_uvp!(vs::Vars, pr::TwoModeParams, g::TwoDGrid, u, v, p)
+function set_uvp!(vs::TwoModeVars, pr::TwoModeParams, g::TwoDGrid, u, v, p)
   uh = fft(u)
   vh = fft(v)
   ph = fft(p)
@@ -881,6 +1230,11 @@ end
 
 function mode0energy(v::PassiveAPVVars, p::TwoModeParams, g::TwoDGrid)
   0.5*FourierFlows.parsevalsum(real.(g.invKKrsq).*abs2.(v.solr[:, :, 1]), g)
+end
+
+function mode0energy(v::PrognosticAPVVars, p::PrognosticAPVParams, g::TwoDGrid)
+  calcPsih!(v, p, g, v.solc, v.solr)
+  0.5*FourierFlows.parsevalsum(real.(g.KKrsq).*abs2.(v.Psih), g)
 end
 
 function mode0energy(prob::AbstractProblem)
@@ -1283,8 +1637,7 @@ Returns the wave-induced x-velocity.
 """
 function wave_induced_u(sig, vs::AbstractVars, pr::AbstractParams, 
   g::AbstractGrid)
-  uw, vw = wave_induced_uv(sig, vs, pr, g)
-  return uw
+  wave_induced_uv(sig, vs, pr, g)[1]
 end
 
 function wave_induced_u(sig, prob::AbstractProblem)
@@ -1295,11 +1648,10 @@ end
 Returns the wave-induced y-velocity. 
 """
 function wave_induced_v(vs, pr, g)
-  uw, vw = wave_induced_uv(sig, vs, pr, g)
-  return vw
+  wave_induced_uv(sig, vs, pr, g)[2]
 end
 
-function wave_induced_v(prob::AbstractProblem)
+wave_induced_v(prob::AbstractProblem)
   wave_induced_v(prob.vars, prob.params, prob.grid)
 end
 
