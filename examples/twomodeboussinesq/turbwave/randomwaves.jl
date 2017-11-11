4904da77510b1f61064b7aa2bf364736a18a63c4
glwagner <wagner.greg@gmail.com>
2017-11-10 20:23:32 -0500

    Working on planar wave example

--- examples/twomodeboussinesq/turbwave/randomwaves.jl
+++ examples/twomodeboussinesq/turbwave/randomwaves.jl
@@ -3,18 +3,23 @@ include("./setup.jl")
 import FourierFlows.TwoModeBoussinesq: mode1speed
 
 # -- Parameters --
-  nkw = 8 
+ name = "planar2strong"
+  nkw = 2
+    ε = 4e-1            # Wave amplitude
+   Ro = 2e-1            # Eddy Rossby number
     n = 384
     L = 2π*1600e3
-    α = 1.0             # Frequency parameter
-    ε = 1e-1            # Wave amplitude
-   Ro = 1e-1            # Eddy Rossby number
- name = "planar8"
+    f = 1e-4
+    N = 5e-3
+   kw = nkw*2π/L
+    m = 1/2000
+    α = (N*kw)^2/(f*m)^2
 
 
 # Setup
-tw, prob, diags, outs = turbwavesetup(name, n, L, α, ε, Ro; nkw=nkw,
-  dtfrac = 1e-2, nperiods = 200, nsubperiods = 1, k0turb = floor(Int, 2n/3), 
+tw, prob, diags, outs = turbwavesetup(name, n, L, α, ε, Ro;
+  nkw=nkw, f=f, N=N, m=m,
+  dtfrac = 2e-2, nperiods = 200, nsubperiods = 1, k0turb = floor(Int, 2n/3), 
   nν0 = 6, nν1 = 6, ν0 = 1e20, ν1 = 1e20,
   nowavecubics = true, wavefield = "planar")
 
