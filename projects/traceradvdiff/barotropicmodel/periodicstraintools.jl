b720eef393f21099c15c98769a580358682132e0
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2017-11-20 16:18:49 -0500

    Working on tracer project

--- projects/traceradvdiff/barotropicmodel/periodicstraintools.jl
+++ projects/traceradvdiff/barotropicmodel/periodicstraintools.jl
@@ -23,18 +23,19 @@ function wavybarotropicrun(ε;
          η = nothing,
          U = 1.0,
        CFL = 0.1,
-        δx = 0.02,
-        δy = 0.02,
-  savefreq = 100,
-  plotfreq = 10,
+        δx = 0.05,
+        δy = 0.05,
+    nsaves = 100,
+    nplots = 100,
       name = "test")
 
+    dx = L/n              # grid spacing
+    dy = H/n              # grid spacing
+    tf = H*L/U            # final time
+  umax = U / (L*(1-ε))    # maximum u-velocity
+  vmax = ε*U/H            # maximum v-velocity
 
-   δ = L/n
-  um = U / (L*(1-ε))
-  tf = H*L/U
-
-  Δt₁ = CFL*δ/um
+  Δt₁ = minimum([CFL*dx/umax, CFL*dy/vmax]) # first guess at timestep
   totsteps₁ = tf/Δt₁
   filename = @sprintf("%s_n%04d_ep%02d.jld2", name, n, 100ε)
 
@@ -42,8 +43,9 @@ function wavybarotropicrun(ε;
   if η == nothing; η = κ; end
 
   # Total steps, intermediate steps.
-  intsteps = ceil(Int, totsteps₁/savefreq) # Number of snapshots and diags
-  totsteps = intsteps*savefreq
+  intsteps = ceil(Int, totsteps₁/nsaves)    # Number of snapshots and diags
+  totsteps = intsteps*nsaves
+  plotfreq = floor(Int, totsteps/nplots)
   Δt = tf/totsteps
 
   # Sinsuidal topography and barotropic velocity
@@ -55,7 +57,7 @@ function wavybarotropicrun(ε;
   ci(x, y) = exp( -(x-x₀)^2/(2*δx^2) - (y-y₀)^2/(2*δy^2) ) / (2π*δx*δy)
 
   # Initialize the problem
-  g = FourierFlows.TwoDGrid(n, L, n, H+ε; y0=-H-ε, effort=FFTW.PATIENT)
+  g = FourierFlows.TwoDGrid(n, L, n, H+ε; y0=-H-ε, effort=FFTW.MEASURE)
   prob = TracerAdvDiff.ConstDiffSteadyFlowProblem(grid=g, κ=κ, η=η, u=u, v=v) 
   TracerAdvDiff.set_c!(prob, ci)
 
@@ -64,32 +66,95 @@ function wavybarotropicrun(ε;
   output = Output(prob, filename, (:c, getc))
 
   # Diagnostics
-  variance(prob) = FourierFlows.cumulant_2y(prob.vars.c, prob.grid)
-  variancediag = Diagnostic(variance, prob)
+  """ Return a vector of the strain tensor calculated at the center of
+  mass of the tracer blob.  Note that since u = U/h, we have
+
+    ux = - U hₓ / h^2        vx = y U ( hₓₓ / h^2 - 2*hₓ^2/h^3 )
+    uy = 0                   vy = u hₓ / h^2  
+
+  where hₓ = ∂h/∂x and hₓₓ = ∂²h/∂x².
+  """
+  h, hₓ, hₓₓ = sinusoidaltopo(ε, H, L)
+  function straintensor(prob)
+    prob.vars.ch .= prob.vars.sol
+    A_mul_B!(prob.vars.c, g.irfftplan, prob.vars.ch)
+
+    c = prob.vars.c
+    g = prob.grid
+
+    x = FourierFlows.cumulant_1x(c, g)
+    y = FourierFlows.cumulant_1y(c, g)
+
+    # Each strain component is a scalar
+    ux = - U*hₓ(x) / h(x)^2
+    vx = y*U * ( hₓₓ(x) / h(x)^2 - 2*hₓ(x)^2/h(x)^3 )
+    vy = U*hₓ(x) / h(x)^2
+    uy = 0.0
+
+    [ux, vx, uy, vy]
+  end
+
+  #variance(prob) = FourierFlows.cumulant_2y(prob.vars.c, prob.grid)
+
+  function variance(prob)
+    prob.vars.ch .= prob.vars.sol
+    A_mul_B!(prob.vars.c, g.irfftplan, prob.vars.ch)
+
+    # Domain average
+    C = g.dx*g.dy*sum(prob.vars.c)
+
+    # Ybar
+    @. prob.vars.cv = g.Y*prob.vars.c
+    c1y = g.dx*g.dy*sum(prob.vars.cv) / C 
+
+    # 2nd cumulant
+    @. prob.vars.cv = (g.Y - c1y)^2.0 * prob.vars.c
+    g.dx*g.dy*sum(prob.vars.cv) / C
+  end
+
+  variance(prob) = 1
+
+  variancediag = Diagnostic(variance, prob; nsteps=totsteps)
+    straindiag = Diagnostic(straintensor, prob; nsteps=totsteps)
+
+  #diags = [variancediag, straindiag]
+  #diags = [straindiag]
   diags = [variancediag]
 
 
   # Plotting
-  fig, ax = subplots(figsize=(8, 4))
   mask = wavychannelmask(ε, H, L, prob.grid)
   
+
   # Integrate
+  msg = "Wavy barotropic run with \n"
+  msg *= @sprintf("   ε: %0.4f\n", ε)
+  msg *= @sprintf("   n: %d\n", n)
+  msg *= @sprintf("   κ: %.2e\n", κ)
+  msg *= @sprintf("   η: %.2e\n", η)
+  msg *= @sprintf("  δx: %.2e\n", δx)
+  msg *= @sprintf("  δy: %.2e\n", δy)
+
+  println(msg)
+
   startwalltime = time()
   println("Running with ε = $ε, n = $n...")
   while prob.step < totsteps
+    @time stepforward!(prob, diags; nsteps=intsteps)
+    #@time stepforward!(prob, nsteps=ntsteps)
+    #saveoutput(output)
 
-    stepforward!(prob, diags; nsteps=intsteps)
-    saveoutput(output)
+    #if prob.step % plotfreq == 0.0
+    #  makeplot(prob, output[:c], mask, filename, save=true)
+    #end
 
-    if prob.step % plotfreq == 0.0
-      makeplot(ax, prob, output[:c], mask, filename, save=true)
-    end
-
-    @printf("frac step: %.2f, walltime = %.2f\n", 
-      prob.step/totsteps, time()-startwalltime)
-
-    
+    #@printf("prog: %.2f, wall: %.2f min, c2: %.2f\n",
+    #  prob.step/totsteps, (time()-startwalltime)/60, 
+    #  variancediag.value/variancediag.data[1])
   end
+
+  #savediagnostic(variancediag, "variance", output.filename)
+  #savediagnostic(straindiag, "strain", output.filename)
 
   nothing
 end
@@ -97,8 +162,9 @@ end
 
 
 
-function makeplot(ax, prob, c, mask, filename; save=false, show=false)
-  axes(ax)
+function makeplot(prob, c, mask, filename; save=false, show=false)
+  close("all")
+  fig, ax = subplots(figsize=(8, 4))
   pcolormesh(prob.grid.X, prob.grid.Y, PyObject(NullableArray(c, mask)))
 
   if save
@@ -121,13 +187,14 @@ Returns u, v functions that define barotropic flow in a wavy channel with
 wave height ε, channel height H, and length L.
 """
 function wavychannelbarotropicflow(ε, H, L, U) 
+
   k₁ = 2π/L      
 
-   h(x) = H*(1 - ε*sin(k₁*x))       # Topography
-  ∂h(x) = -H*k₁*ε*cos(k₁*x)        # Topographic x-gradient
+   h(x) = H*(1 - ε*sin(k₁*x))      # Topography
+  hₓ(x) = -H*k₁*ε*cos(k₁*x)        # Topographic x-gradient
 
   u(x, y) = U/h(x)                 # Barotropic x-velocity
-  v(x, y) = y*u(x, y)*∂h(x)/h(x)   # Topography-following z-velocity
+  v(x, y) = y*u(x, y)*hₓ(x)/h(x)   # Topography-following z-velocity
 
   u, v
 end
@@ -135,8 +202,24 @@ end
 
 
 
-function wavychannelmask(ε, H, L, grid)
+function sinusoidaltopo(ε, H, L)
+  k₁ = 2π/L
 
+  # Sinusoidal topo and its derivatives
+     h(x) = H*(1 - ε*sin(k₁*x))      
+    hₓ(x) = -H*k₁*ε*cos(k₁*x)       
+   hₓₓ(x) = H*k₁^2*ε*sin(k₁*x)     
+
+  h, hₓ, hₓₓ 
+end
+
+
+
+
+"""
+Returns an array of booleans that masks sinusoidal topography.
+"""
+function wavychannelmask(ε, H, L, grid)
   k₁ = 2π/L      
   h(x) = H*(1 - ε*sin(k₁*x))       # Topography
 
--- projects/traceradvdiff/barotropicmodel/wavychannelsuite.jl
+++ projects/traceradvdiff/barotropicmodel/wavychannelsuite.jl
@@ -1,13 +1,16 @@
 include("./periodicstraintools.jl")
 
 name = "try_1"
- n = 1024
- κ = 2e-4
+ n =  256
+ κ = 1e-4
  η = 1e-4
-δx = 0.02
-δy = 0.04
+δx = 0.01
+δy = 0.01
+CFL = 0.01
 
+ε = 0.2
+wavybarotropicrun(ε, n=n, name=name, κ=κ, η=η, δx=δx, δy=δy, CFL=CFL)
 
-for ε in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-  wavybarotropicrun(ε, name=name, κ=κ, η=η, δx=δx, δy=δy)
-end
+#for ε in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+#  wavybarotropicrun(ε, n=n, name=name, κ=κ, η=η, δx=δx, δy=δy, CFL=CFL)
+#end
