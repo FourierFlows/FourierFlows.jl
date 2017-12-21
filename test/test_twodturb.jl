5ebeb417167366d0687fa69c0509d2388c5fb3ff
Navid Constantinou <navidcy@gmail.com>
2017-12-20 20:03:51 -0800

    adds Filtered time steppers in test_twodturb.jl

--- test/test_twodturb.jl
+++ test/test_twodturb.jl
@@ -1,4 +1,4 @@
-include("../src/FourierFlows.jl")
+include("../src/fourierflows.jl")
 
 using Base.Test
 using FourierFlows
@@ -17,14 +17,37 @@ end
 function teststepforward(g, p, v, eq; dt=1e-16, nsteps=10,
   stepper="ForwardEuler")
 
-  if stepper == "ForwardEuler"; ts = ForwardEulerTimeStepper(dt, eq.LC)
-  elseif stepper == "AB3";      ts = AB3TimeStepper(dt, eq.LC)
-  elseif stepper == "RK4";      ts = RK4TimeStepper(dt, eq.LC)
-  elseif stepper == "ETDRK4";   ts = ETDRK4TimeStepper(dt, eq.LC)
+  ts = FilteredForwardEulerTimeStepper(dt, g, v; filterorder=4.0, innerfilterK=0.4, outerfilterK=1)
+  filter = ts.filter
+
+  if stepper == "ForwardEuler"
+      ts = ForwardEulerTimeStepper(dt, eq.LC)
+  elseif stepper == "FiltrForwardEuler"
+      ts = FilteredForwardEulerTimeStepper(dt, g, v)
+  elseif stepper == "AB3"
+      ts = AB3TimeStepper(dt, eq.LC)
+  elseif stepper == "RK4"
+      ts = RK4TimeStepper(dt, eq.LC)
+  elseif stepper == "ETDRK4"
+      ts = ETDRK4TimeStepper(dt, eq.LC)
+  elseif stepper == "FiltrETDRK4"
+      ts = FilteredETDRK4TimeStepper(dt, eq.LC, g)
   end
 
+  # qi = rand(g.nx, g.ny)
+  # qi = sin.(3*g.X).*sin.(2*g.Y) + 2*sin.(1*g.X).*sin.(4*g.Y)
+  # qi = qi-mean(qi)
+  # if stepper == "FiltrForwardEuler" || stepper == "FiltrETDRK4"
+  #   qi = irfft(rfft(qi).*ts.filter, g.nx)
+  # end
+
+
+  qih = (randn(g.nkr, g.nl)+im*randn(g.nkr, g.nl)).*filter
+  qih[1, 1]=0
+  qi = irfft(qih, g.nx)
+
   prob = Problem(g, v, p, eq, ts)
-  TwoDTurb.set_q!(prob, rand(g.nx, g.ny))
+  TwoDTurb.set_q!(prob, qi)
 
   absq₀ = sum(abs.(prob.vars.sol))
   stepforward!(prob; nsteps=nsteps)
@@ -40,7 +63,9 @@ end
 
 @testset "TwoDTurb and Timestepper Tests" begin
   @test teststepforward(128, 2π, 1e-2, 2; stepper="ForwardEuler")
+  @test teststepforward(128, 2π, 1e-2, 2; stepper="FiltrForwardEuler")
   @test teststepforward(128, 2π, 1e-2, 2; stepper="AB3")
   @test teststepforward(128, 2π, 1e-2, 2; stepper="RK4")
   @test teststepforward(128, 2π, 1e-2, 2; stepper="ETDRK4")
+  @test teststepforward(128, 2π, 1e-2, 2; stepper="FiltrETDRK4")
 end
