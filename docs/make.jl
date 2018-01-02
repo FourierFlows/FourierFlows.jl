afb767859ab831d30fb67e3b8131e09d6ac2d779
Navid Constantinou <navidcy@gmail.com>
2018-01-02 10:46:29 -0800

    some fixes in docs make

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,6 @@ makedocs(modules=[FourierFlows],
         doctest=true)
 
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/navidcy/FourierFlows.jl.git",
+    repo = "github.com/FourierFlows/FourierFlows.jl.git",
     julia  = "0.6.0",
     osname = "linux")
