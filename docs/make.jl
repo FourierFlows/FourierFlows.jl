e5d3385fc3d6a8eabb3fef08f9d94e7169cfaba1
Navid Constantinou <navidcy@gmail.com>
2018-01-06 08:36:23 -0800

    adds page structure for docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,18 @@ makedocs(modules=[FourierFlows],
          format = :html,
          sitename = "navidccc",
          pages = Any[
-                  "Home" => "index.md"
-                  ]
-)
+          "Home" => "index.md",
+          "Tutorials" => Any[
+              "tutorials/page1.md",
+              "tutorials/page2.md",
+              "tutorials/page3.md"
+          ],
+          "section2" => Any[
+              "sec2/page1.md",
+              "sec2/page2.md",
+              "sec2/page3.md"
+          ]
+          ])
 
  deploydocs(
      repo   = "github.com/FourierFlows/FourierFlows.jl.git",
