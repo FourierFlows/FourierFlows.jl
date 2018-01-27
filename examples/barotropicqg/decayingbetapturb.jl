f85dfab4a503ec3d128127049768995d580794e1
Navid Constantinou <navidcy@gmail.com>
2018-01-26 21:57:18 -0800

    finalizes decaying beta-plane turbulence example

--- examples/barotropicqg/decayingbetapturb.jl
+++ examples/barotropicqg/decayingbetapturb.jl
@@ -5,17 +5,17 @@ import FourierFlows.BarotropicQG: energy, enstrophy
 
 # Physical parameters
 nx  = 256
-ν  = 0e-05
-νn = 1
-f0 = 1.0
+Lx = 2π     # domain size
+ν  = 0e-05  # viscosity
+νn = 1      # viscosity order
 
-β = 20.0
-Lx = 2π
-μ = 0e-1
-F = 0.0
+f0 = 1.0    # Coriolis parameter
+β = 15.0    # planetary PV gradient
+μ = 0e-1    # bottom drag
 
-FU(t) = F
-η(x, y) = 0*x
+F = 0.0     # large-scale flow U(t) forcing;
+FU(t) = F   # not applicable here
+η(x, y) = 0*x   # bottom topography
 
 g  = BarotropicQG.Grid(nx, Lx)
 p  = BarotropicQG.Params(g, f0, β, FU, η, μ, ν, νn)
@@ -25,15 +25,14 @@ eq = BarotropicQG.Equation(p, g)
 
 
 # Time-stepping
-dt = 0.005
-nsteps = 20000
-nsubs  = 1000
+dt = 0.02
+nsteps = 8000
+nsubs  = 500
 
 
 ts = FourierFlows.autoconstructtimestepper("FilteredETDRK4", dt, eq.LC, g)
 prob = FourierFlows.Problem(g, v, p, eq, ts)
 s = prob.state
-
 
 
 # Files
@@ -47,22 +46,17 @@ if isfile(filename); rm(filename); end
 if !isdir(plotpath); mkdir(plotpath); end
 
 
-g = prob.grid
 
-# Initial condition closely following pyqg barotropic example
-# that reproduces the results of the paper by McWilliams (1984)
+
+# Initial condition that has power only at wavenumbers with
+# 8<L/(2π)*sqrt(kx^2+ky^2)<10 and initial energy E0
 srand(1234)
-k0, E0 = 6, 0.1
-modk = sqrt.(g.KKrsq)
-psik = zeros(g.nk, g.nl)
-psik =  (modk.^2 .* (1 + (modk/k0).^4)).^(-0.5)
-psik[1, 1] = 0.0
-psik = ones(g.nk, g.nl)
-psik =  (modk.^2 .* (1 + (modk/k0).^4)).^(-0.5)
-psik[real(g.KKrsq).<(8*2*pi/g.Lx)^2]=0
-psik[real(g.KKrsq).>(10*2*pi/g.Lx)^2]=0
-psik[1, :]=0
-psih = (randn(g.nkr, g.nl)+im*randn(g.nkr, g.nl)).*psik
+E0 = 0.1
+modk = ones(g.nkr, g.nl)
+modk[real(g.KKrsq).<(8*2*pi/g.Lx)^2]=0
+modk[real(g.KKrsq).>(10*2*pi/g.Lx)^2]=0
+modk[1, :]=0
+psih = (randn(g.nkr, g.nl)+im*randn(g.nkr, g.nl)).*modk
 psih = psih.*prob.ts.filter
 Ein = real(sum(g.KKrsq.*abs2.(psih)/(g.nx*g.ny)^2))
 psih = psih*sqrt(E0/Ein)
@@ -70,8 +64,6 @@ qi = -irfft(g.KKrsq.*psih, g.nx)
 E0 = FourierFlows.parsevalsum(g.KKrsq.*abs2.(psih), g)
 
 BarotropicQG.set_zeta!(prob, qi)
-BarotropicQG.updatevars!(prob)
-
 
 # Create Diagnostic -- "energy" is a function imported at the top.
 E = Diagnostic(energy, prob; nsteps=nsteps)
@@ -90,7 +82,8 @@ out = Output(prob, filename, (:sol, get_sol), (:u, get_u))
 
 
 function plot_output(prob, fig, axs; drawcolorbar=false)
-  # Plot the vorticity field and the evolution of energy and enstrophy.
+  # Plot the vorticity and streamfunction fields as well as the zonal mean
+  # vorticity and the zonal mean zonal velocity.
 
   s, v, p, g = prob.state, prob.vars, prob.params, prob.grid
   BarotropicQG.updatevars!(prob)
@@ -99,7 +92,8 @@ function plot_output(prob, fig, axs; drawcolorbar=false)
   cla()
   pcolormesh(g.X, g.Y, v.q)
   axis("square")
-  axis("off")
+  # axis("off")
+  title(L"vorticity $\zeta$")
   if drawcolorbar==true
     colorbar()
   end
@@ -108,7 +102,8 @@ function plot_output(prob, fig, axs; drawcolorbar=false)
   cla()
   pcolormesh(g.X, g.Y, v.psi)
   axis("square")
-  axis("off")
+  # axis("off")
+  title(L"streamfunction $\psi$")
   if drawcolorbar==true
     colorbar()
   end
@@ -116,12 +111,18 @@ function plot_output(prob, fig, axs; drawcolorbar=false)
   sca(axs[3])
   cla()
   plot(mean(v.zeta, 1).', g.Y[1,:])
-  # axis("square")
+  plot(0*g.Y[1,:], g.Y[1,:], "k--")
+  ylim(-Lx/2, Lx/2)
+  xlim(-2, 2)
+  title(L"mean $\zeta$")
 
   sca(axs[4])
   cla()
   plot(mean(v.u, 1).', g.Y[1,:])
-  # axis("square")
+  plot(0*g.Y[1,:], g.Y[1,:], "k--")
+  ylim(-Lx/2, Lx/2)
+  xlim(-0.5, 0.5)
+  title(L"mean $u$")
 
   pause(0.001)
 end
@@ -139,17 +140,19 @@ while prob.step < nsteps
 
   # Message
   log = @sprintf("step: %04d, t: %d, E: %.4f, Q: %.4f, τ: %.2f min",
-    prob.step, prob.t, E.value/E.data[1], Z.value/Z.data[1],
+    prob.step, prob.t, E.value, Z.value,
     (time()-startwalltime)/60)
 
   println(log)
 
   plot_output(prob, fig, axs; drawcolorbar=false)
-
 end
+
+# how long did it take?
 println((time()-startwalltime))
 
 plot_output(prob, fig, axs; drawcolorbar=false)
 
+# save the figure as png
 savename = @sprintf("%s_%09d.png", joinpath(plotpath, plotname), prob.step)
-savefig(savename, dpi=240)
+savefig(savename)
