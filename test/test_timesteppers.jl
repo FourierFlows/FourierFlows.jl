61fb1b8e20ba13b0914d3e718c025dade5b55a3f
Navid Constantinou <navidcy@gmail.com>
2018-02-06 15:28:28 -0800

    adds a test for final energy in test_timesteppers.jl

--- test/test_timesteppers.jl
+++ test/test_timesteppers.jl
@@ -1,4 +1,5 @@
 import FourierFlows.TwoDTurb
+import FourierFlows.TwoDTurb: energy
 
 # Dictionary of (stepper, nsteps) pairs to test. Each stepper is tested by
 # stepping forward nstep times.
@@ -17,16 +18,21 @@ Build a twodturb initial value problem and use it to test time-stepping methods.
 We integrate a random IC from 0 to t=tf.
 The amplitude of the initial condition is kept low (e.g. multiplied by 1e-5)
 so that nonlinear terms do not come into play. This way we can compare the final
-state qh(t) with qh(0)*exp(-ν k^2 t).
+state qh(t=tf) with qh(t=0)*exp(-ν k^nν tf).
+We choose to linear drag (nν=0) so that we can test the energy since in that
+case E(t=tf) = E(t=0)*exp(-2ν tf).
 """
-function testtwodturbstepforward(n=64, L=2π, ν=1e-3, nν=1;
+function testtwodturbstepforward(n=64, L=2π, ν=1e-2, nν=0;
                                  nsteps=100, stepper="ForwardEuler")
 
   dt = tf/nsteps
 
   prob = TwoDTurb.InitialValueProblem(nx=n, Lx=L, ny=n, Ly=L, ν=ν, nν=nν, dt=dt,
                                         stepper=stepper)
-  g, p, s = prob.grid, prob.params, prob.state
+  g, p, v, s = prob.grid, prob.params, prob.vars, prob.state
+
+  E = Diagnostic(energy, prob, nsteps=nsteps)
+  diags = [E]
 
   # Initial condition (IC)
   qih = (rand(g.nkr, g.nl) + im*rand(g.nkr, g.nl))*1e-5
@@ -42,12 +48,15 @@ function testtwodturbstepforward(n=64, L=2π, ν=1e-3, nν=1;
   q0h = deepcopy(qih)
 
   TwoDTurb.set_q!(prob, qi)
+  E0 = energy(s, v, g)
 
-  stepforward!(prob, nsteps)
+  stepforward!(prob, diags, nsteps)
+
   qfh = deepcopy(q0h).*exp.(-p.ν*g.KKrsq.^p.nν.*s.step*dt)
+   Ef = E0.*exp.(-2*p.ν*s.step*dt)
 
   # println(sum(abs.(qfh-s.sol)) / sum(abs.(qfh)))
-  isapprox(qfh, prob.state.sol, rtol=n^2*nsteps*1e-13)
+  isapprox(qfh, prob.state.sol, rtol=n^2*nsteps*1e-13) &  isapprox(Ef, E[E.count], rtol=n^2*nsteps*1e-13)
 end
 
 
