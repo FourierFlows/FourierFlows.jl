1f0033e566a2d967e9a35fe623c6fe8782348b8a
Navid Constantinou <navidcy@gmail.com>
2018-03-08 20:55:59 -0800

    adds AB3 and FilteredAB3 tests for Rossby wave in barotropicqg

--- test/test_barotropicqg.jl
+++ test/test_barotropicqg.jl
@@ -5,7 +5,10 @@ import FourierFlows.BarotropicQG
 
 """ Test that the time-stepper is not doing anything wild by evolving a random
 initial condition for dt=1e-16 looking at relative error of the norm. """
-function test_baroQG_RossbyWave(prob, nsteps, dt)
+function test_baroQG_RossbyWave(stepper, dt, nsteps, g, p, v, eq)
+
+    ts = FourierFlows.autoconstructtimestepper(stepper, dt, eq.LC, g)
+    prob = FourierFlows.Problem(g, v, p, eq, ts)
 
     s, v, p, g = prob.state, prob.vars, prob.params, prob.grid
 
@@ -23,8 +26,8 @@ function test_baroQG_RossbyWave(prob, nsteps, dt)
     BarotropicQG.updatevars!(prob)
 
     ζ_theory = ampl*cos.(kwave*(g.X-ω/kwave*s.t)).*cos.(lwave*g.Y)
-
-    isapprox(ζ_theory, v.zeta, rtol=g.nx*g.ny*nsteps*1e-15)
+    # println(norm(ζ_theory - v.zeta)/norm(ζ_theory))
+    isapprox(ζ_theory, v.zeta, rtol=g.nx*g.ny*nsteps*1e-12)
 end
 
 
@@ -33,13 +36,11 @@ end
 # Running the tests
 
 nx  = 64
-dt  = 1e-3
-nsteps = 200
 ν  = 0.0
 νn = 2
 
 f0 = 1.0
- β = 0.2
+ β = 2.0
 Lx = 2π
  μ = 0.0
 
@@ -51,7 +52,26 @@ p  = BarotropicQG.Params(g, f0, β, FU, η, μ, ν, νn)
 v  = BarotropicQG.Vars(g)
 eq = BarotropicQG.Equation(p, g)
 
-ts = FourierFlows.autoconstructtimestepper("ETDRK4", dt, eq.LC, g)
-prob = FourierFlows.Problem(g, v, p, eq, ts)
+dt, nsteps  = 1e-2, 20
+@test test_baroQG_RossbyWave("ETDRK4", dt, nsteps, g, p, v, eq)
 
-@test test_baroQG_RossbyWave(prob, nsteps, dt)
+dt, nsteps  = 1e-2, 20
+@test test_baroQG_RossbyWave("FilteredETDRK4", dt, nsteps, g, p, v, eq)
+
+dt, nsteps  = 1e-2, 20
+@test test_baroQG_RossbyWave("RK4", dt, nsteps, g, p, v, eq)
+
+dt, nsteps  =1e-2, 20
+@test test_baroQG_RossbyWave("FilteredRK4", dt, nsteps, g, p, v, eq)
+
+dt, nsteps  = 1e-3, 200
+@test test_baroQG_RossbyWave("AB3", dt, nsteps, g, p, v, eq)
+
+dt, nsteps  = 1e-3, 200
+@test test_baroQG_RossbyWave("FilteredAB3", dt, nsteps, g, p, v, eq)
+
+dt, nsteps  = 1e-4, 2000
+@test test_baroQG_RossbyWave("ForwardEuler", dt, nsteps, g, p, v, eq)
+#
+dt, nsteps  = 1e-4, 2000
+@test test_baroQG_RossbyWave("FilteredForwardEuler", dt, nsteps, g, p, v, eq)
