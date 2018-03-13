8eb7ba1fe0274ef64586c202aaf639b10e28bb41
Navid Constantinou <navidcy@gmail.com>
2018-03-13 16:10:09 -0700

    minor polishing in the twodturbtests comments [ci skip]

--- test/test_twodturb.jl
+++ test/test_twodturb.jl
@@ -136,7 +136,7 @@ function testnonlinearterms( dt, stepper; n = 128, L=2π, ν=1e-2, nν=1,
 # Assume a solution ψ_guess for the unforced case. Then insert the solution
 # in the equation and compute the remainder analytically, e.g.:
 # remainder = -(∂ζ_guess/∂t + J(ψ_guess, ζ_guess) - νΔζ_guess)
-# Finally we use remainder as forcing. Then ζ_guess should be a solution of
+# Finally use remainder as forcing. Then ζ_guess should be a solution of
 # the forced problem.
 
   n, L  = 128, 2π
