6b394187bb3838352b2aa01b611ae2b9f8094806
Navid Constantinou <navidcy@gmail.com>
2018-07-14 14:49:06 +1000

    finalizes cellular flow (constant in t) example

--- examples/traceradvdiff/cellularflow.jl
+++ examples/traceradvdiff/cellularflow.jl
@@ -5,54 +5,36 @@ import FourierFlows.TracerAdvDiff
 # Numerical parameters and time-stepping parameters
 nx  = 128      # 2D resolution = nx^2
 stepper = "RK4"   # timestepper
-dt  = 0.005     # timestep
-nsteps = 100 # total number of time-steps
-nsubs  = 50   # number of time-steps for plotting
+dt  = 0.02     # timestep
+nsteps = 5000 # total number of time-steps
+nsubs  = 200   # number of time-steps for plotting
                # (nsteps must be multiple of nsubs)
 
 # Physical parameters
-Lx  = 2π       # domain size
-
+Lx  = 2π      # domain size
+κ   = 0.002   # diffusivity
 
 gr = TwoDGrid(nx, Lx)
 X, Y = gr.X, gr.Y
 
+# streamfunction (for plotting) and (u,v) flow field
 psiampl = 0.2
 m, n = 1, 1
 psiin = psiampl*cos.(m*X).*cos.(n*Y)
 uin(x, y) = +psiampl*n*cos.(m*x).*sin.(n*y)
 vin(x, y) = -psiampl*m*sin.(m*x).*cos.(n*y)
 
-# uvel, vvel = 0.2, 0.1
-# psiin = -uvel*Y + vvel*X
-# uin(x, y) = uvel + 0*x
-# vin(x, y) = vvel + 0*x
-
 
 prob = TracerAdvDiff.ConstDiffSteadyFlowProblem(;
-  grid = nothing,
-    nx = nx,
-    Lx = Lx,
-    ny = nothing,
-    Ly = nothing,
-     κ = 0.00,
-     η = nothing,
-     u = uin,
-     v = vin,
-    dt = dt,
-  stepper = stepper
-  )
+    nx = nx, Lx = Lx, κ = κ, u = uin, v = vin, dt = dt, stepper = stepper)
 
 s, v, p, g, eq, ts = prob.state, prob.vars, prob.params, prob.grid, prob.eqn, prob.ts;
 
 
-σ = 0.1
-c0func(x, y) = 0.1*exp.(-(x.^2+y.^2)/(2σ^2))
-# + 0*ones(size(g.X)) + 0*sin.(1/2*g.X).*sin.(1/2*g.Y)
-
-c0 = c0func.(g.X, g.Y)
-tf = nsteps*dt
-cf = c0func.(g.X-uvel*tf, g.Y-vvel*tf)
+# initial condition c0 = c(x, y, t=0)
+amplc0, sigc0 = 0.1, 0.1
+c0func(x, y) = amplc0*exp.(-(x.^2+y.^2)/(2sigc0^2))
+c0 = c0func.(g.X-1.2, g.Y)
 
 
 TracerAdvDiff.set_c!(prob, c0)
@@ -60,7 +42,7 @@ TracerAdvDiff.set_c!(prob, c0)
 
 function plot_output(prob, fig, axs; drawcolorbar=false)
 
-  # Plot the PV field and the evolution of energy and enstrophy.
+  # Plot the concentration field and the (u, v) streamlines
 
   s, v, p, g = prob.state, prob.vars, prob.params, prob.grid
   t = round(prob.state.t, 2)
@@ -69,7 +51,9 @@ function plot_output(prob, fig, axs; drawcolorbar=false)
   pcolormesh(g.X, g.Y, v.c)
   if drawcolorbar;colorbar();end
   contour(g.X, g.Y, psiin, 15, colors="k", linewidths=0.7)
-  title("\$c(x,y,\\mu t= $t )\$")
+  axis("equal")
+  axis("square")
+  title("shading: \$c(x, y, t= $t )\$, contours: \$\\psi(x, y)\$")
   pause(0.001)
 end
 
