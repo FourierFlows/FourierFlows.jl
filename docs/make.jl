cf5f26e69a7a52935dd6e4cdc0c1b28518bab978
Navid Constantinou <navidcy@gmail.com>
2018-08-17 14:55:54 +1000

    change julia version to 0.7

--- docs/make.jl
+++ docs/make.jl
@@ -27,7 +27,7 @@ makedocs(
 deploydocs(
        repo = "github.com/FourierFlows/FourierFlows.jl.git",
      target = "build",
-      julia = "0.6.3",
+      julia = "0.7",
      osname = "linux",
        deps = nothing,
        make = nothing
