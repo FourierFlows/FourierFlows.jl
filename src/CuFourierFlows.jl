703bc54cc72b17c635d9b0bbd3c5934d7c6550ca
Gregory Wagner <wagner.greg@gmail.com>
2019-07-01 15:13:18 -0400

    adds CuFourierFlows

---
+++ src/CuFourierFlows.jl
@@ -0,0 +1,8 @@
+using CuArrays
+
+# Discard `effort` argument for CuArrays
+plan_flows_fft(a::CuArray, effort) = plan_fft(a)
+plan_flows_rfft(a::CuArray, effort) = plan_rfft(a)
+
+OneDGrid(dev::GPU, args...; kwargs...) = OneDGrid(args...; ArrayType=CuArray, kwargs...)
+TwoDGrid(dev::GPU, args...; kwargs...) = TwoDGrid(args...; ArrayType=CuArray, kwargs...)
