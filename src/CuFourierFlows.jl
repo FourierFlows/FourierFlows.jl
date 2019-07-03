e733c95653380683e8da22641f934cb1322dfc32
Gregory Wagner <wagner.greg@gmail.com>
2019-07-03 16:36:25 -0400

    adds zeros and ArrayType for GPU devices

--- src/CuFourierFlows.jl
+++ src/CuFourierFlows.jl
@@ -6,3 +6,12 @@ plan_flows_rfft(a::CuArray, effort) = plan_rfft(a)
 
 OneDGrid(dev::GPU, args...; kwargs...) = OneDGrid(args...; ArrayType=CuArray, kwargs...)
 TwoDGrid(dev::GPU, args...; kwargs...) = TwoDGrid(args...; ArrayType=CuArray, kwargs...)
+
+function Base.zeros(::GPU, T, dims)
+    a = CuArray{T}(undef, dims...)
+    a .= 0
+    return a
+end
+
+ArrayType(::GPU, T, dim) = CuArray{T, dim}
+ArrayType(::GPU) = CuArray
